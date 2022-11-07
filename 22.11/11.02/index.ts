2a3c79ad57632809bcfc9cb39ce08290d8c20f13
HanGyeol9931 <jn365@naver.com>
2022-11-07 15:06:35 +0900

    블록체인 완성

---
+++ 22.11/11.02/index.ts
@@ -0,0 +1,46 @@
+import { P2PServer } from "./src/core/server/p2p";
+import express from "express"
+import { BlockChain } from "@core/index";
+
+const app = express();
+const ws = new P2PServer();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("bit-chain")
+})
+
+// 블록 내용 조회
+app.get("/chains",(req,res)=>{
+    res.json(ws.getChain());
+})
+
+app.post("/mineBlcok",(req,res)=>{
+    const { data } = req.body;
+    const newBlock = ws.addBlock(data);
+    if(newBlock.isError) return res.send(newBlock.value);
+    res.json(newBlock.value);
+})
+
+// P2PServer 웹소켓 연결 요청
+app.post("/addToPeer",(req,res)=>{
+    const { peer } = req.body;
+    ws.connectToPeer(peer);
+});
+
+// 연결된 socket 조회
+app.get("/peer",(req,res)=>{
+    const sockets = ws.getSockets().map((socket : any)=>{
+        return res.json(socket)
+    })
+});
+app.listen(8000,()=>{
+    console.log("서버열림");
+    ws.listen();
+});
+
+
+
+// ts-node로 열어야 서버 열린다.
+
--- 22.11/11.02/src/core/blockChain/chain.ts
+++ 22.11/11.02/src/core/blockChain/chain.ts
@@ -24,6 +24,39 @@ export class Chain {
         this.blockchain.push(newBlock);
         return{isError:false,value :newBlock}
     }
+    // 체인 검증 코드
+    public isValidChain(chain : Block[]) : Failable<undefined,string>{
+        // 최초 블록 검사 하는 코드
+        const genesis = chain[0]
+
+        for (let i = 0; i < chain.length; i++) {
+            const newBlcok = chain[i];
+            const previousBlock = chain[i-1];
+            const isValid =Block.isValidNewBlock(newBlcok,previousBlock);
+            if(isValid.isError) return{isError:true,value : isValid.value}
+        }
+        return { isError : false , value :undefined};
+    }
+    
+    public replaceChain(receivedChain : Block[]):Failable<undefined,string>{
+        // 본인 체인과 상대방 체인을 검사하는 과정
+        const latestReceiveBlock : Block = receivedChain[receivedChain.length-1]
+        const latestBlock : Block = this.getLatestBlock();
+        if(latestReceiveBlock.height === 0){
+            return {isError : true ,value : "받은 블록이 최초 블록"}
+        }
+        if(latestReceiveBlock.height <= latestBlock.height){
+            return {isError : true, value : "본인의 블록보다 길거나 같은 블록"}
+        }
+        if(latestReceiveBlock.previousHash === latestBlock.hash){
+            return {isError : true,value: "블록이 하나 만큼 모자라다"}
+        }
+
+        // 체인을 갱신 해준다.
+        this.blockchain = receivedChain;
+
+        return {isError : false, value : undefined}
+    }
 
     // 생성 시점기준으로 블록 높이  -10인 블록 구하기
 
